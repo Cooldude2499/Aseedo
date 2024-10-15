90e998482d6022915779fbf91f853aa972516963
Cooldude2499 <130143592+Cooldude2499@users.noreply.github.com>
2024-10-16 05:12:59 +0530

    updated app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const  businessOwnerRoutes = require('./routes/signUp/businessOwner');
 const MongoStore = require("connect-mongo");
 
 // const dbUrl = 'mongodb://localhost:27017/aseedo';     // For development
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/aseedo';
+const dbUrl = process.env.DB_URL;
 
 
 main().catch((err) => console.log(err));
